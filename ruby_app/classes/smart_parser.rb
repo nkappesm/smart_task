3522d9b01c86e31227eb3dfcd68d4d863e118444
Nicolas Kappes <nkappesm@gmail.com>
2022-02-24 01:34:30 -0300

    smart task classes structure

---
+++ ruby_app/classes/smart_parser.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This class parses a webserver log file and outputs different ordered lists
+class SmartParser
+  def initialize(smart_log)
+    @smart_log = smart_log
+  end
+
+  def most_viewed
+  end
+
+  def unique_most_viewed
+  end
+end
