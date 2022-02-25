993281ade88e703e4d548157eecdc33f8d63e0a6
Map <nkappesm@gmail.com>
2022-02-25 16:49:37 -0300

    initial tests

--- spec/classes/smart_parser_spec.rb
+++ spec/classes/smart_parser_spec.rb
@@ -5,16 +5,21 @@ require_relative '../../ruby_app/classes/smart_parser'
 
 RSpec.describe SmartParser do
   context 'with correct data' do
+    let(:test_log) { instance_double('SmartLog', result: { 'home/1' => ['1.1.1.1', '1.1.1.2'], 'a/1' => ['1.1.2.2'] }) }
+    let(:parser) { SmartParser.new(test_log) }
 
     it 'outputs page views ordered' do
+      expect(parser.most_viewed).to eq([['home/1', 2], ['a/1', 1]])
     end
 
     it 'outputs most unique views ordered' do
+      expect(parser.most_unique_viewed).to eq([['home/1', 2], ['a/1', 1]])
     end
   end
 
   context 'with incorrect data' do
-    it 'raises an exception' do
+    xit 'raises an exception' do
+      # expect { parser.most_viewed }.to raise_error(SyntaxError)
     end
   end
 end
