9c6742a3314a5602c1b350246d9d943db48af992
Map <nkappesm@gmail.com>
2022-02-26 01:24:13 -0300

    smart parses ordering methods

--- ruby_app/classes/smart_parser.rb
+++ ruby_app/classes/smart_parser.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
-# This class parses a webserver log file and outputs different ordered lists
+# Parses a SmartLog and outputs a variety of processed data
 class SmartParser
   def initialize(smart_log)
     @smart_log = smart_log
   end
 
   def most_viewed
+    routes_visits = @smart_log.result.transform_values(&:count)
+    routes_visits.sort_by { |_, visits| visits }.reverse
   end
 
-  def unique_most_viewed
+  def most_unique_viewed
+    routes_unique_visits = @smart_log.result.transform_values(&:uniq).transform_values(&:count)
+    routes_unique_visits.sort_by { |_, visits| visits }.reverse
   end
 end
